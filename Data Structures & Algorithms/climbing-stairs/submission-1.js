bcf07e1595093631b26942a7f15db45a6e11da07
Karituo-Luster <55764170+Karituo-Luster@users.noreply.github.com>
2026-06-07 11:31:35 -0700

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        let ways = [];
+        ways[1] = 1;
+        ways[2] = 2;
+        for(let i = 3; i <= n; i++){
+            ways[i] = ways[i-1] + ways[i-2];
+        }
+        return ways[n];
+    }
+}
